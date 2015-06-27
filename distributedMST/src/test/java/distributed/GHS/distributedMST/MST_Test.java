1a64424ae1bdd8a9ff084363c671cb825e2de6dc
Anuj Verma <anuj.mnit71@gmail.com>
2015-06-28 03:25:40 +0530

    commit GHS

---
+++ distributedMST/src/test/java/distributed/GHS/distributedMST/MST_Test.java
@@ -0,0 +1,38 @@
+package distributed.GHS.distributedMST;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MST_Test 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MST_Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MST_Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
